3cdbac05a848c595894ada5a0a7d2851c80d012d
Ruslan Zhykin <r.zhykin@haab.eu>
2016-07-05 16:12:36 +0300

    fix win

--- parser.js
+++ parser.js
@@ -3,11 +3,6 @@ var globArr = [];
 var lng = {};
 var count_ru = 0, count_all =0;
 var text =  {};
- //{'GUID кат':'***GUID cat',
-// 'GUID категории вставки':'***GUID of spot catogory',
-// 'GUID клипа':'***GUID of clip',
-// 'GUID передачи':'***GUID of program',
-// 'GUID прем':'***GUID prem#'};
 
 var en = fs.readFileSync('src/en.csv', 'utf8');
 var en_arr = en.split("\r\n");
@@ -15,7 +10,6 @@ for(var i in en_arr){
     var tmp_arr = en_arr[i].split(";");
     text[tmp_arr[0]]=tmp_arr[1];
 }
-//console.log(text);
 
 var contents = fs.readFileSync('src/lng/tpChannel_ru.lng', 'utf8');
 var arr = contents.split("\r\n");
@@ -24,19 +18,16 @@ for(var i in arr){
     lng[tmp[0]]=tmp[1];
 
      if (tmp[1] in text){
-        //console.log(tmp[0] + " - " + text[tmp[1]] );
         count_all++;
-        // globArr[tmp[0]]=text[tmp[1]];
+
         if (text[tmp[1]].search(/[А-яЁё]/) > 0 ){
             globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]];
-            //console.log(globArr[tmp[0]]);
+
             count_ru++;
         }
         else {
             text[tmp[1]] = text[tmp[1]].replace("***","")
-
             globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]];
-            //console.log(text[tmp[1]]);
         }
 
     }else globArr[tmp[0]] = tmp[0];
