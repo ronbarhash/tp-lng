7259eb2ca774203e4da663a4f0f16790747e6344
Ruslan Zhykin <r.zhykin@haab.eu>
2016-07-05 17:23:59 +0300

    refactoring

--- parser.js
+++ parser.js
@@ -2,38 +2,54 @@ var fs = require('fs');
 var globArr = [];
 var lng = {};
 var count_ru = 0, count_all =0;
-var text =  {};
+var ifile = 'src/en.csv';
+var ofile = 'src/lng/tpChannel_ru.lng';
 
-var en = fs.readFileSync('src/en.csv', 'utf8');
-var en_arr = en.split("\r\n");
-for(var i in en_arr){
-    var tmp_arr = en_arr[i].split(";");
-    text[tmp_arr[0]]=tmp_arr[1];
+function file4Arr(fileName, separate){
+    var arr =  {};
+    var f = fs.readFileSync(fileName, 'utf8');
+    var f_arr = f.split("\r\n");
+    for(var i in f_arr){
+        var tmp_arr = f_arr[i].split(separate);
+        arr[tmp_arr[0]]=tmp_arr[1];
+    }
+    return arr;
 }
+//
+// var en = fs.readFileSync('src/en.csv', 'utf8');
+// var en_arr = en.split("\r\n");
+// for(var i in en_arr){
+//     var tmp_arr = en_arr[i].split(";");
+//     text[tmp_arr[0]]=tmp_arr[1];
+// }
 
-var contents = fs.readFileSync('src/lng/tpChannel_ru.lng', 'utf8');
-var arr = contents.split("\r\n");
-for(var i in arr){
-    var tmp = arr[i].split("=");
-    lng[tmp[0]]=tmp[1];
+var text = file4Arr(ifile, ";");
 
-     if (tmp[1] in text){
+// var contents = fs.readFileSync('src/lng/tpChannel_ru.lng', 'utf8');
+// var arr = contents.split("\r\n");
+// for(var i in arr){
+//     var tmp = arr[i].split("=");
+//     lng[tmp[0]]=tmp[1];
+
+var lng = file4Arr(ofile,"=");
+
+
+for(var i in lng){ //
+     if (lng[i] in text){
         count_all++;
-
-        if (text[tmp[1]].search(/[А-яЁё]/) > 0 ){
-            globArr[tmp[0]] = tmp[0] + "=" + tmp[0];
-            // globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]]; // AAAA
-
+        if (text[lng[i]].search(/[А-яЁё]/) > 0 ){
+            //globArr[i] = i + "=" + i; // miCaptions
+            globArr[i] = i + "=" + text[lng[i]]; // RU ***
             count_ru++;
         }
         else {
-            text[tmp[1]] = text[tmp[1]].replace("***","")
-            globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]];
+            text[lng[i]] = text[lng[i]].replace("***","")
+            globArr[i] = i + "=" + text[lng[i]];
         }
 
-    }else globArr[tmp[0]] = tmp[0];
-    if(j === globArr[j])
-        globArr[j]="";
+    }else globArr[i] = i;
+    if(i === globArr[i])
+        globArr[i]="";
 
 }
 console.log(globArr);
@@ -42,7 +58,5 @@ for (var j in globArr){
         fs.appendFileSync('dist/tpChannel_en.lng', "\n" + globArr[j] + '\r\n','ascii' );
     else
         fs.appendFileSync('dist/tpChannel_en.lng', globArr[j] + '\r\n' );
-
-    //console.log(j+globArr[j]);
 }
 console.log("RU =" + count_ru + " and ALL = " + count_all);
