1e0889129ca29d3bf8889a74239c3c0936daa5d7
Ruslan Zhykin <r.zhykin@haab.eu>
2016-07-05 14:40:22 +0300

    change structure

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var globArr = [];
 var lng = {};
-
- var text =  {};
+var count_ru = 0, count_all =0;
+var text =  {};
  //{'GUID кат':'***GUID cat',
 // 'GUID категории вставки':'***GUID of spot catogory',
 // 'GUID клипа':'***GUID of clip',
 // 'GUID передачи':'***GUID of program',
 // 'GUID прем':'***GUID prem#'};
 
-var en = fs.readFileSync('d:/en.csv', 'utf8');
+var en = fs.readFileSync('en.csv', 'utf8');
 var en_arr = en.split("\r\n");
 for(var i in en_arr){
     var tmp_arr = en_arr[i].split(";");
@@ -17,7 +17,7 @@ for(var i in en_arr){
 }
 // console.log(text);
 
-var contents = fs.readFileSync('d:/test.lng', 'utf8');
+var contents = fs.readFileSync('test.txt', 'utf8');
 var arr = contents.split("\r\n");
 for(var i in arr){
     var tmp = arr[i].split("=");
@@ -25,21 +25,25 @@ for(var i in arr){
 
      if (tmp[1] in text){
         //console.log(tmp[0] + " - " + text[tmp[1]] );
-
+        count_all++;
         // globArr[tmp[0]]=text[tmp[1]];
         if (text[tmp[1]].search(/[А-яЁё]/) > 0 ){
             globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]];
+            console.log(globArr[tmp[0]]);
+            count_ru++;
         }
         else {
             text[tmp[1]] = text[tmp[1]].replace("***","")
             globArr[tmp[0]] = tmp[0] + "=" + text[tmp[1]];
-            console.log(text[tmp[1]]);
+            //console.log(text[tmp[1]]);
         }
 
-}else globArr[tmp[0]] = tmp[0];
+    }else globArr[tmp[0]] = tmp[0];
+
 
 }
-    for (var j in globArr){
-    fs.appendFile('d:/test.en.txt', globArr[j] + '\r\n' , function (err) {
-    });
+for (var j in globArr){
+    fs.appendFile('./tpChannel_en.lng', globArr[j] + '\r\n' , function (err) {
+});
 }
+console.log("RU =" + count_ru + " and ALL = " + count_all);
